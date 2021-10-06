1f1fcf9b7dc07a10ffe1da398752293dc1cc125b
ajtock <andytock@gmail.com>
2021-10-06 17:04:14 +0100

    extract geneIDs

---
+++ Cvi/syri_variant_calling/ajt200_run_v160921/REF_Col_QRY_Cvi/extract_SnpSift_ann_genes.R
@@ -0,0 +1,29 @@
+#!/applications/R/R-4.0.0/bin/Rscript
+
+# Usage:
+# ./extract_SnpSift_ann_genes.R TAIR10_chr_all_renamed_fa_headers Cvi.chr.all.v2.0_renamed_fa_headers HIGH
+
+#ref <- "TAIR10_chr_all_renamed_fa_headers"
+#qry <- "Cvi.chr.all.v2.0_renamed_fa_headers"
+#ann <- "HIGH"
+
+args <- commandArgs(trailingOnly = T)
+ref <- args[1]
+qry <- args[2]
+ann <- args[3]
+
+library(stringr)
+
+vcf <- read.table(paste0(ref, "_", qry, "_syri_filt.SnpSift_intervals_", ann, ".vcf"),
+                  header = T, skip = 37, comment.char = "")
+
+# HIGH
+geneIDs <- str_extract_all(vcf$INFO, paste0(gsub("_.+", "", ann), "\\|AT\\dG[0-9]+"))
+geneIDs <- unique(gsub(paste0(gsub("_.+", "", ann), "\\|"), "", unlist(geneIDs)))
+
+# LOF
+geneIDs <- str_extract_all(vcf$INFO, paste0(gsub("_.+", "", ann), "=\\(AT\\dG[0-9]+"))
+geneIDs <- unique(gsub(paste0(gsub("_.+", "", ann), "=\\("), "", unlist(geneIDs)))
+
+print(geneIDs)
+
